97e06fcd3278b53a431001150fa1a841395f645a
Djordje <djordje_v@hotmail.com>
2025-01-13 13:05:47 -0800

    Add favicon stuff

--- apps/web/lib/datastructures.ts
+++ apps/web/lib/datastructures.ts
@@ -104,8 +104,7 @@ export const testimonials = [
     src: "https://media.licdn.com/dms/image/v2/D5603AQGrknAcOfFewQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1728346713809?e=1741824000&v=beta&t=HmpznviPcn_xjZN-pDvdqmX8ZTf6-Wg-rGI-7ZIx7_M",
   },
   {
-    quote:
-      "Djordje was an absolute pleasure to work with. His work ethic was top-tier, consistently going above and beyond to ensure that every assignment was completed to the highest standard. Djordje is a sharp critical thinker who approaches problems thoughtfully, finding creative and effective solutions, rather than just mindlessly completing assigned work.",
+    quote: "[Insert nice & fun quote from Rose here]",
     name: "Rose Kuan",
     designation: "Senior Product Designer, Coinbase",
     src: "https://media.licdn.com/dms/image/v2/D4E03AQHZCxkLSRRk9A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696544456905?e=1741824000&v=beta&t=Jog917Zkbyp9OFdu_hBTAyHfZRqU4scIy8qKm_QCjjE",
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -11,6 +11,31 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pixels & Bits",
   description: "Product Engineering & Design Studio",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
   openGraph: {
     title: "Pixels & Bits",
     description: "Product Engineering & Design Studio",
